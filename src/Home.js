c00e928818c9968abd5f380ad80c18a6a4cf5978
alexwebcoder <alexdeleon@stayup.com>
2023-05-30 13:53:03 -0700

    practiced useState hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const Home = () => {
+    
+    const [name, setName] = useState('Coronado');
+
+    const handleClick = (e) => {
+        setName('Colon');
+    }
+
+    
+    const [message, setMessage] = useState('');
+
+     const [color, setColor] = useState('red');
+     
     return ( 
         <div className="home">
-            <h2> Homepage</h2>
+            <h2>Homepage</h2>
+            <p>{name}</p>
+            <p>{message}</p>
+            <p>`My favorite color is {color}`</p>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={ () => setColor('Green')}>Green</button>
+            
+            <input type="text" 
+            value={message} 
+            placeholder="Enter a message" 
+            onChange={e => setMessage(e.target.value)} />
         </div>
      );
 }
