5cd7a26ee04398e6ba96327bccb15c8223b31bdd
alexwebcoder <alexdeleon@stayup.com>
2023-05-31 13:40:35 -0700

    added props to the Bloglist component

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,16 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipusm...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipusm...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipusm...', author: 'mario', id: 3 }
-    ]);
+      ]);
 
     return ( 
         <div className="home">
-          {blogs.map((blog) => (
-            <div className="blog-preview">
-              <h2>{ blog.title }</h2>
-              <p>Written by: { blog.author }</p>
-            </div>
-          
-          ))}
+          <BlogList blogs={blogs} title="All Blogs!!"/>
         </div>
      );
 }
