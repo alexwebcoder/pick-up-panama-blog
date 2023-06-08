43d8b62cfee0fa580b2a6dce0388c9d4c07c1110
alexwebcoder <alexdeleon@stayup.com>
2023-06-08 14:43:06 -0700

    added route parameters

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const BlogDetails = () => {
+ //using the useParams hook to grab values (route params) from current url (route)
+    const { id } = useParams();
+
     return (
         <div className="blog-details">
             <h2>
-                Blog Details
-            </h2>
+                Blog Details - {id}
+            </h2> 
         </div>
 
     );
