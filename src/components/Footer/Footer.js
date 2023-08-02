f757398564169fbc388d39c5cef331dcecb89333
alexwebcoder <alexdeleon@stayup.com>
2023-08-02 10:15:00 -0700

    fixed className on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
     return ( 
         <footer className="footer">
         <div className="icons">
-           <a href="#"><i class="fab fa-facebook"></i></a>
-           <a href="#"><i class="fab fa-linkedin"></i></a>
-           <a href="#"><i class="fab fa-instagram"></i></a>
-           <a href="#"><i class="fab fa-twitter"></i></a>
+           <a href="#"><i className="fab fa-facebook"></i></a>
+           <a href="#"><i className="fab fa-linkedin"></i></a>
+           <a href="#"><i className="fab fa-instagram"></i></a>
+           <a href="#"><i className="fab fa-twitter"></i></a>
             <p className="company-name">
                 ABC &copy; 2021, ALL Rights Reserved
             </p>
