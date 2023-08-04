e80b9204870488d140e53b58d14c3208925b8ef2
alexwebcoder <alexdeleon@stayup.com>
2023-08-04 13:41:50 -0700

    added form component to footer

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
                  <Route path='/*' element={ <NotFound /> } />
               </Routes>
           </div>
-          {/* <Footer /> */}
+          <Footer />
        </div>
     </Router>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,23 @@
+import ContactForm from "../ContactForm/ContactForm";
 import "./Footer.css"
 
 const Footer = () => {
     return ( 
-        <footer className="footer">
-        <div className="icons">
-           <a href="#"><i className="fab fa-facebook"></i></a>
-           <a href="#"><i className="fab fa-linkedin"></i></a>
-           <a href="#"><i className="fab fa-instagram"></i></a>
-           <a href="#"><i className="fab fa-twitter"></i></a>
-            <p className="company-name">
-                ABC &copy; 2021, ALL Rights Reserved
-            </p>
-        </div>
+        <>
+        <footer  id="footer-nav" className="footer centered">
+        <ContactForm />
+        <nav className="icons">
+           <a href="/"><i className="fab fa-facebook"></i></a>
+           <a href="/"><i className="fab fa-linkedin"></i></a>
+           <a href="/"><i className="fab fa-instagram"></i></a>
+           <a href="/"><i className="fab fa-twitter"></i></a>     
+        </nav>
+        <p className="copyright">
+            &copy; Pick Up Panama 2023, All rights reserved. <br/>Content on PickUpPanama.com may not be reproduced without written permission.
+        </p>
+        <p className="credits">Articles and Photography by Alex DeLeon</p>
     </footer>
+        </>
      );
 }
  
