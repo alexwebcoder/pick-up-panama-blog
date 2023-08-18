aa74f706541bcd6d6883fb22693192b862a49dc2
alexwebcoder <alexdeleon@stayup.com>
2023-08-18 11:08:33 -0700

    added axios and newsfeed component

--- src/pages/Newsfeed/Newsfeed.js
+++ src/pages/Newsfeed/Newsfeed.js
@@ -1,6 +1,25 @@
-import "./Newsfeed.css"
+import { useEffect, useState } from "react";
+import "./Newsfeed.css";
+import axios from "axios";
 
 const Newsfeed = () => {
+  const [articles, setArticles] = useState([]);
+ 
+  console.log(articles);
+
+  const getArticles = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/");
+      setArticles(res.data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+   getArticles();
+  }, []);
+
     return (
       <>
         <h1>Newsfeed</h1>
