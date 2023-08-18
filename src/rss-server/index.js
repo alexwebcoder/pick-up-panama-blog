e5ab68feced36bff63d69c83669f269e8ece9603
alexwebcoder <alexdeleon@stayup.com>
2023-08-18 10:46:54 -0700

    added server for rss feed

---
+++ src/rss-server/index.js
@@ -0,0 +1,31 @@
+import cors from "cors";
+import express from "express";
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
+import RSSParser from "rss-parser";
+
+const feedURL = "https://www.einnews.com/rss/NCx-iF5DlIFJE0fC";
+const parser = new RSSParser();
+let articles = [];
+const parse = async url => {
+  const feed = await parser.parseURL(url);
+
+
+  feed.items.forEach(item => {
+    articles.push({ item });
+  })
+}
+
+parse(feedURL);
+
+let app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(articles);
+})
+
+const server = app.listen("4000", () => {
+    console.log('App is Listenting');
+});
+
+export default server;
