1c7af036df900ec7b31c3ae2d9e43638010ce335
alexwebcoder <alexdeleon@stayup.com>
2023-08-20 20:27:25 -0700

    added more styling to the slider

--- src/pages/Home/Slider/Slider.js
+++ src/pages/Home/Slider/Slider.js
@@ -7,39 +7,39 @@ const Slider = () => {
         <div className="line-divider centered wide">
                 <h2 className="diamond-text-box">
                 <span className="left"></span>
-                <span className="content caps centered">Facts</span>
+                <span className="content caps centered">Facts about Panama</span>
                 <span className="right"></span>
                 </h2>
         </div>
         <div className="slider-container">
             <div className="slider">
                 <div className="slides">
-                <div id="slides__1" className="slide">
-                    <span className="slide__text">Panama's official name is The Republic of Panama.</span>
-                    <a className="slide__prev" href="#slides__4" title="Next"></a>
-                    <a className="slide__next" href="#slides__2" title="Next"></a>
+                    <div id="slides-1" className="slide">
+                        <span className="slide-text">Panama's official name is The Republic of Panama.</span>
+                        <a className="slide-prev" href="#slides-4" title="Next"></a>
+                        <a className="slide-next" href="#slides-2" title="Next"></a>
+                    </div>
+                    <div id="slides-2" className="slide">
+                        <span className="slide-text">The population of Panama is 3,800,644.</span>
+                        <a className="slide-prev" href="#slides-1" title="Prev"></a>
+                        <a className="slide-next" href="#slides-3" title="Next"></a>
+                    </div>
+                    <div id="slides-3" className="slide">
+                        <span className="slide-text">The Capital is Panama City.</span>
+                        <a className="slide-prev" href="#slides-2" title="Prev"></a>
+                        <a className="slide-next" href="#slides-4" title="Next"></a>
+                    </div>
+                    <div id="slides-4" className="slide">
+                        <span className="slide-text">Panama has a rainy season and a dry season.</span>
+                        <a className="slide-prev" href="#slides-3" title="Prev"></a>
+                        <a className="slide-next" href="#slides-1" title="Prev"></a>
+                    </div>
                 </div>
-                <div id="slides__2" className="slide">
-                    <span className="slide__text">The population of Panama is 3,800,644.</span>
-                    <a className="slide__prev" href="#slides__1" title="Prev"></a>
-                    <a className="slide__next" href="#slides__3" title="Next"></a>
-                </div>
-                <div id="slides__3" className="slide">
-                    <span className="slide__text">The Capital is Panama City.</span>
-                    <a className="slide__prev" href="#slides__2" title="Prev"></a>
-                    <a className="slide__next" href="#slides__4" title="Next"></a>
-                </div>
-                <div id="slides__4" className="slide">
-                    <span className="slide__text">Panama has a rainy season and a dry season.</span>
-                    <a className="slide__prev" href="#slides__3" title="Prev"></a>
-                    <a className="slide__next" href="#slides__1" title="Prev"></a>
-                </div>
-                </div>
-                <div className="slider__nav">
-                <a className="slider__navlink" href="#slides__1"></a>
-                <a className="slider__navlink" href="#slides__2"></a>
-                <a className="slider__navlink" href="#slides__3"></a>
-                <a className="slider__navlink" href="#slides__4"></a>
+                <div className="slider-nav">
+                <a className="slider-navlink" href="#slides-1"></a>
+                <a className="slider-navlink" href="#slides-2"></a>
+                <a className="slider-navlink" href="#slides-3"></a>
+                <a className="slider-navlink" href="#slides-4"></a>
                 </div>
             </div>
         </div>
