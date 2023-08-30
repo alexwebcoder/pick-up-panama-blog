257c8d5d286e9112f8b2c9060b4c4bdfd1528a69
alexwebcoder <alexdeleon@stayup.com>
2023-08-30 12:07:57 -0700

    changed urls to ids and started adding to json file

--- src/pages/Home/BlogDetails.js
+++ src/pages/Home/BlogDetails.js
@@ -1,5 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
 import useFetch from '../../useFetch';
+import "./BlogDetails.css";
 
 const BlogDetails = () => {
  //using the useParams hook to grab values (route params) from current url (route)
@@ -21,13 +22,44 @@ const BlogDetails = () => {
             { isPending && <div>Loading...</div> } 
             { error && <div>{ error }</div> }
             { blog && (
-                <article>
+                <article className={blog.className}>
                     <h2>{ blog.title }</h2>
+                    <figure className="panel-1">
+                        <img className="no-right-click" src={blog.image} alt={blog.alt}/>
+                        <figcaption>{blog.figcaption}</figcaption>
+                    </figure>
                     <p>Written by: { blog.author }</p>
                     <div>
                         { blog.body }
                     </div>
-                    <button onClick={handleClick}>Delete Blog</button>
+                    <figure className="panel-1">
+                        <img className="no-right-click" src={blog.imageTwo} alt={blog.altTwo}/>
+                        <figcaption>{blog.figcaptionTwo}</figcaption>
+                    </figure>
+                    <div>
+                        { blog.bodyTwo }
+                    </div>
+                    <figure className="panel-1">
+                        <img className="no-right-click" src={blog.imageThree} alt={blog.altThree}/>
+                        <figcaption>{blog.figcaptionThree}</figcaption>
+                    </figure>
+                    <div>
+                        { blog.bodyThree }
+                    </div>
+                    <figure className="panel-1">
+                        <img className="no-right-click" src={blog.imageFour} alt={blog.altFour}/>
+                        <figcaption>{blog.figcaptionFour}</figcaption>
+                    </figure>
+                    <div>
+                        { blog.bodyFour }
+                    </div>
+                    <figure className="panel-1">
+                        <img className="no-right-click" src={blog.imageFive} alt={blog.altFive}/>
+                        <figcaption>{blog.figcaptionFive}</figcaption>
+                    </figure>
+                    <div>
+                        { blog.bodyFive }
+                    </div>
                 </article>
             )}
         </div>
