3435dae5c7a805c58929f08c524f928a84d4b9f4
alexwebcoder <alexdeleon@stayup.com>
2023-09-05 13:42:25 -0700

    added text into json file and blog details template

--- src/pages/Home/BlogDetails.js
+++ src/pages/Home/BlogDetails.js
@@ -13,48 +13,46 @@ const BlogDetails = () => {
     Capitalize();
 
     return (
-        <main className="blog-details margin">
+        <main className={`blog-details margin `}>
             { isPending && <div>Loading...</div> } 
             { error && <div>{ error }</div> }
             { blog && (
                 <section className={blog.className}>
                     <p className="date centered">{ blog.date }</p>
                     <h1 className="centered">{ blog.title }</h1>
-                    <h2 className="body">
+                    <p className="body">
                         { blog.body }
-                    </h2>
-                    <figure>
-                        <img className="no-right-click" src={blog.imageTwo} alt={blog.altTwo}/>
-                        <figcaption>{blog.figcaptionTwo}</figcaption>
-                    </figure>
-                    <div>
+                    </p>
+                    <p className="body-two">
                         { blog.bodyTwo }
+                    </p>
+                    <div className="body-two-a">
+                        { blog.bodyTwoA }
                     </div>
-                    <figure>
-                        <img className="no-right-click" src={blog.imageThree} alt={blog.altThree}/>
-                        <figcaption>{blog.figcaptionThree}</figcaption>
-                    </figure>
-                    <div>
-                        { blog.bodyThree }
-                    </div>
-                    <figure>
-                        <img className="no-right-click" src={blog.imageFour} alt={blog.altFour}/>
-                        <figcaption>{blog.figcaptionFour}</figcaption>
-                    </figure>
-                    <div>
-                        { blog.bodyFour }
-                    </div>
-                    <figure>
-                        <img className="no-right-click" src={blog.imageFive} alt={blog.altFive}/>
-                        <figcaption>{blog.figcaptionFive}</figcaption>
-                    </figure>
-                    <div>
-                    <figure>
-                        <img className="no-right-click" src={blog.image} alt={blog.alt}/>
-                        <figcaption>{blog.figcaption}</figcaption>
-                    </figure>
-                        { blog.bodyFive }
-                    </div>
+                    <section>
+                        <h2>{ blog.h2 }</h2>
+                        <p>{ blog.bodyThree }</p>
+                        <h3>{ blog.h3 }</h3>
+                        <p>{ blog.bodyFour }</p>
+                        <p>{ blog.bodyFourA }</p>
+                        <h3>{ blog.h3One }</h3>
+                        <p>{ blog.bodyFive }</p>
+                        <p>{ blog.bodySix }</p>
+                        <p>{ blog.bodySeven }</p>
+                        <p>{ blog.bodyEight }</p>
+                        <p>{ blog.bodyNine }</p>
+                        <p>{ blog.bodyTen }</p>
+                        <h3>{ blog.h3Two }</h3>
+                        <p>{ blog.bodyEleven }</p>
+                        <p>{ blog.bodyTwelve }</p>
+                        <p>{ blog.bodyThirteen }</p>
+                        <h2>{ blog.h2One }</h2>
+                        <p>{ blog.bodyFourteen }</p>
+                        <p>{ blog.bodyFifteen }</p>
+                        <p>{ blog.bodySixteen }</p>
+                        <h2>{ blog.h2Two }</h2>
+
+                    </section>
                 </section>
             )}
         </main>
