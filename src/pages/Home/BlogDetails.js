a0bcc28c47141864624ce7dd13a970ce90e4f4b4
alexwebcoder <alexdeleon@stayup.com>
2023-09-05 17:14:25 -0700

    added in the rest of the text to the json file

--- src/pages/Home/BlogDetails.js
+++ src/pages/Home/BlogDetails.js
@@ -51,6 +51,64 @@ const BlogDetails = () => {
                         <p>{ blog.bodyFifteen }</p>
                         <p>{ blog.bodySixteen }</p>
                         <h2>{ blog.h2Two }</h2>
+                        <p>{ blog.bodySeventeen }</p>
+                        <ul>
+                            <li>{ blog.listItemA}</li>
+                            <li>{ blog.listItemB}</li>
+                            <li>{ blog.listItemC}</li>
+                        </ul>
+                        <p>{ blog.bodyEighteen }</p>
+                        <p>{ blog.bodyNineteen }</p>
+                        <p>{ blog.bodyTwenty }</p>
+                        <h3>{ blog.h3Three }</h3>
+                        <p>{ blog.bodyTwentyOne }</p>
+                        <h3>{ blog.h3Four }</h3>
+                        <p>{ blog.bodyTwentyTwo }</p>
+                        <p>{ blog.bodyTwentyThree }</p>
+                        <h2>{ blog.h2Three }</h2>
+                        <p>{ blog.bodyTwentyFour }</p>
+                        <p>{ blog.bodyTwentyFive }</p>
+                        <p>{ blog.bodyTwentySix }</p>
+                        <h2>{ blog.h2Four }</h2>
+                        <p>{ blog.bodyTwentySeven }</p>
+                        <h2>{ blog.h2Five }</h2>
+                        <ul>
+                            <li>{ blog.listItemD}</li>
+                            <li>{ blog.listItemE}</li>
+                            <li>{ blog.listItemF}</li>
+                            <li>{ blog.listItemG}</li>
+                            <li>{ blog.listItemH}</li>
+                            <li>{ blog.listItemI}</li>
+                            <li>{ blog.listItemJ}</li>
+                            <li>{ blog.listItemK}</li>
+                            <li>{ blog.listItemL}</li>
+                            <li>{ blog.listItemM}</li>
+                            <li>{ blog.listItemN}</li>
+                            <li>{ blog.listItemO}</li>
+                            <li>{ blog.listItemP}</li>
+                            <li>{ blog.listItemQ}</li>
+                            <li>{ blog.listItemR}</li>
+                            <li>{ blog.listItemS}</li>
+                            <li>{ blog.listItemT}</li>
+                            <li>{ blog.listItemU}</li>
+                        </ul>
+                        <p>{ blog.bodyTwentyEight }</p>
+                        <p>{ blog.bodyTwentyNine }</p>
+
+
+
+
+                        
+
+
+
+
+
+
+
+
+
+                        
 
                     </section>
                 </section>
