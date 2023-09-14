7bf2ebe659e0f0e4997fbc927af170c15964b71d
alexwebcoder <alexdeleon@stayup.com>
2023-09-13 18:17:37 -0700

    reworded about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -23,28 +23,28 @@ const About = () => {
                 <figure className="panel-1">
                     <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/alex-deleon-panama.JPG'} alt='Alex in Panama City'/>
                 </figure>
-                <p>My name is Alex. I am a Panamanian American and have traveled to Panama multiple times.</p>
+                <p>My name is Alex and I am a Panamanian American who has been to Panama several times.</p>
                 <figure className="panel-2">
                     <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/man-and-woman-on-boat.JPG'} alt='A man and a woman on a boat'/>
                 </figure>
                 <figure className="panel-3">
                     <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/san-blas-indians.JPG'} alt='San Blas Indians selling items'/>
                 </figure>
-                <p>When we travel out of the country, we have the opportunity to learn from other cultures.</p>
+                <p>We learn about other cultures by visiting foreign countries.</p>
                 <figure className="panel-3a">
                     <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/sea-turtle.JPG'} alt='A sea turtle swimming'/>
                 </figure>
                 <figure className="panel-4">
                     <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/ship-in-the-harbor.JPG'} alt='A ship in the Panama harbor'/>
                 </figure>
-                <p>What we learn from traveling becomes clear when we return home.</p>
+                <p>When we get home, the lessons we learned while traveling become clear-cut.</p>
                 <figure className="panel-5">
                     <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/a-sea-anemone.JPG'} alt='A sea anemone'/>
                 </figure>
                 <figure className="panel-6">
                     <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/young-lady-walking.JPG'} alt='A young lady walking in front of street vendors'/>
                 </figure>
-                <p className="based-on">Based on my trips to Panama, I created Pick Up Panama to answer questions about Panama.</p>
+                <p className="based-on">Based on my trips to Panama and my Panamanian heritage, I created Pick Up Panama to share my experiences and answer questions about Panama.</p>
                 <figure className="panel-7">
                     <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/sea-food-meal.JPG'} alt='A seafood meal from the fish market'/>
                 </figure>
