9e35fdfe51c2f51392332b4d76fce1be5875c254
alexwebcoder <alexdeleon@stayup.com>
2023-09-17 21:00:36 -0700

    about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
     return(
         <main className="about-page">
             <figure className="about-hero">
-                <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/coronado-panama.JPG'} alt="Villa Mayte Coronado Panama"/>
+                <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/about/coronado-panama.JPG'} alt="Villa Mayte Coronado Panama"/>
                 <figcaption className="fig-caption">Villa Mayte Vacation property in Coronado Panama</figcaption>
             </figure>
             <div className="line-divider centered wide">
@@ -21,32 +21,32 @@ const About = () => {
             </div>
             <div className="comic">
                 <figure className="panel-1">
-                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/alex-deleon-panama.JPG'} alt='Alex in Panama City'/>
+                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/about/alex-deleon-panama.JPG'} alt='Alex in Panama City'/>
                 </figure>
                 <p>My name is Alex and I am a Panamanian American who has been to Panama several times.</p>
                 <figure className="panel-2">
-                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/man-and-woman-on-boat.JPG'} alt='A man and a woman on a boat'/>
+                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/about/man-and-woman-on-boat.JPG'} alt='A man and a woman on a boat'/>
                 </figure>
                 <figure className="panel-3">
-                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/san-blas-indians.JPG'} alt='San Blas Indians selling items'/>
+                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/about/san-blas-indians.JPG'} alt='San Blas Indians selling items'/>
                 </figure>
                 <p>We learn about other cultures by visiting foreign countries.</p>
                 <figure className="panel-3a">
-                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/sea-turtle.JPG'} alt='A sea turtle swimming'/>
+                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/about/sea-turtle.JPG'} alt='A sea turtle swimming'/>
                 </figure>
                 <figure className="panel-4">
-                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/ship-in-the-harbor.JPG'} alt='A ship in the Panama harbor'/>
+                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/about/ship-in-the-harbor.JPG'} alt='A ship in the Panama harbor'/>
                 </figure>
                 <p>When we get home, the lessons we learned while traveling become clear-cut.</p>
                 <figure className="panel-5">
-                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/a-sea-anemone.JPG'} alt='A sea anemone'/>
+                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/about/a-sea-anemone.JPG'} alt='A sea anemone'/>
                 </figure>
                 <figure className="panel-6">
-                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/young-lady-walking.JPG'} alt='A young lady walking in front of street vendors'/>
+                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/about/young-lady-walking.JPG'} alt='A young lady walking in front of street vendors'/>
                 </figure>
                 <p className="based-on">Based on my trips to Panama and my Panamanian heritage, I created Pick Up Panama to share my experiences and answer questions about Panama.</p>
                 <figure className="panel-7">
-                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/sea-food-meal.JPG'} alt='A seafood meal from the fish market'/>
+                    <img className="no-right-click" src={process.env.PUBLIC_URL + '/images/about/sea-food-meal.JPG'} alt='A seafood meal from the fish market'/>
                 </figure>
             </div>
         </main>
