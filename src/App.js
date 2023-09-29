5197b714fa3d0459a92a9ac799999428d785a86a
alexwebcoder <alexdeleon@stayup.com>
2023-09-28 18:58:12 -0700

    added working search box

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import { useState } from "react";
 import Navbar from './components/Header/Navbar';
 import Home from './pages/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BlogDetails from './pages/Home/BlogDetails';
 import NotFound from './pages/NotFound/NotFound';
-import SearchBar from './components/SearchBar/SearchBar';
-import SearchResults from './components/SearchResults/SearchResult';
 import Footer from "./components/Footer/Footer";
 import About from "./pages/About/About";
 import Newsfeed from "./pages/Newsfeed/Newsfeed";
 import ContactUs from './pages/ContactUs/ContactUs';
 import FeatureStory from './pages/FeatureStory/FeatureStory';
 import ScrollTop from './components/ScrollTop/ScrollTop';
+import Search from './components/Search/Search';
+import SearchData from './components/Search/searchData.json';
 
 function App() {
-  const [results, setResults] = useState([]);
   return (
     <Router>
        <div className="App">
           <Navbar />
-          <SearchBar setResults={setResults}/>
-          <SearchResults results={results} />
+          <Search placeholder='search' data={SearchData}/>
               <Routes>
                  <Route path='/' element={ <Home/> } />
                  <Route path='/newsfeed' element={ <Newsfeed/> } />
