26ebd6bae540832b0a9bc7399ea8b7ae269da4bc
alexwebcoder <alexdeleon@stayup.com>
2023-09-28 17:08:49 -0700

    reverted back search component

--- src/components/Searchbar/SearchBar.js
+++ src/components/Searchbar/SearchBar.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 // import useFetch from '../../../src/useFetch';
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css"
-import { SearchResults } from "semantic-ui-react";
 
 
 const SearchBar = ({ setResults }) => {
@@ -10,15 +9,15 @@ const SearchBar = ({ setResults }) => {
     
 
     const fetchData = (value) => {
-      fetch('http://localhost:8000/searchResults/')
+      fetch('http://localhost:8000/blogs/')
        .then((response) => response.json())
        .then((json) => {
-         const results = json.filter((searchResults) => {
+         const results = json.filter((blogs) => {
             return (
                 value && 
-                SearchResults && 
-                SearchResults.siteData && 
-                SearchResults.siteData.toLowerCase().includes(value)
+                blogs && 
+                blogs.title && 
+                blogs.title.toLowerCase().includes(value)
             );
          });
          setResults(results);
