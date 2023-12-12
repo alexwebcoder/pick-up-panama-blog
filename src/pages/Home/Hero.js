ecdf3733a96b383d5f0f47d75448d4df3c4db15a
alexwebcoder <alexdeleon@stayup.com>
2023-12-11 19:40:40 -0800

    changed 380 to 320

--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
         <div className="hero-image">
             <figure>
                 <img fetchpriority="high" className='no-right-click'
-                srcSet={process.env.PUBLIC_URL + '/images/home/panama-sign-1x.webp 380w, /images/home/panama-sign-2x.webp 640w, /images/home/panama-sign.webp 2543w,'}
+                srcSet={process.env.PUBLIC_URL + '/images/home/panama-sign-1x.webp 320w, /images/home/panama-sign-2x.webp 640w, /images/home/panama-sign.webp 2543w,'}
                 src={process.env.PUBLIC_URL + '/images/home/panama-sign.webp'} alt="Alex DeLeon in front of the Panama sign"/>
             </figure>
         </div>
