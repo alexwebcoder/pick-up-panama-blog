f0df136a593fc73001f43cf279b3dd2d997a6669
alexwebcoder <alexdeleon@stayup.com>
2023-12-11 17:50:30 -0800

    added tablet and mobile hero images and added srcSet attribute

--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -3,7 +3,9 @@ const Hero = () => {
     return (
         <div className="hero-image">
             <figure>
-                <img fetchpriority="high" className='no-right-click' src={process.env.PUBLIC_URL + '/images/home/panama-sign.webp'} alt="Alex DeLeon in front of the Panama sign"/>
+                <img fetchpriority="high" className='no-right-click'
+                srcSet={process.env.PUBLIC_URL + '/images/home/panama-sign-1x.webp 380w, /images/home/panama-sign-2x.webp 640w, /images/home/panama-sign.webp 2543w,'}
+                src={process.env.PUBLIC_URL + '/images/home/panama-sign.webp'} alt="Alex DeLeon in front of the Panama sign"/>
             </figure>
         </div>
     )
