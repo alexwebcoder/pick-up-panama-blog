47fc3f8ee9c1c33d41b9bf990b46673efbabe73b
alexwebcoder <alexdeleon@stayup.com>
2023-12-28 16:17:06 -0800

    added breakpoints on bloglist component

--- src/pages/Home/BlogList.js
+++ src/pages/Home/BlogList.js
@@ -7,13 +7,31 @@ const handleArticleChange = () => {
 }
 
 const BlogList = ({ blogs, title }) => {
+  if(window.screen.width >= 321) {
     return(
       <div className="card-container">
         {blogs.map((blog) => (
           <article key={blog.id}>
               <Link to={`/blogs/${blog.id}`} onClick={handleArticleChange}>
               <figure className="card-img">
-                        <img className="no-right-click" src={blog.thumbnail} alt={blog.alt}/>
+              <picture>
+                  <source
+                    type="image/webp"
+                    srcSet={
+                      blog.Thum492w
+                    }
+                
+                    sizes="(max-width: 320px) 320px, 492px"
+                  />
+                  <source
+                    type="image/jpg"
+                    srcSet={
+                      blog.Thum492j
+                    }
+                    sizes="(max-width: 320px) 320px, 492px"
+                  />
+                        <img className="no-right-click" src={blog.Thum492j} alt={blog.alt}/>
+                </picture>
               </figure>
                 <div className="blog-article-content">
                   <h3>{ blog.title }</h3>
@@ -23,6 +41,40 @@ const BlogList = ({ blogs, title }) => {
           ))}
       </div>
     );
+  }
+  if(window.screen.width <= 320) {
+    <div className="card-container">
+        {blogs.map((blog) => (
+          <article key={blog.id}>
+              <Link to={`/blogs/${blog.id}`} onClick={handleArticleChange}>
+              <figure className="card-img">
+              <picture>
+                  <source
+                    type="image/webp"
+                    srcSet={
+                      blog.Thum320w
+                    }
+                
+                    sizes="(max-width: 320px) 320px, 492px"
+                  />
+                  <source
+                    type="image/jpg"
+                    srcSet={
+                      blog.Thum320j
+                    }
+                    sizes="(max-width: 320px) 320px, 492px"
+                  />
+                        <img className="no-right-click" src={blog.Thum320j} alt={blog.alt}/>
+                </picture>
+              </figure>
+                <div className="blog-article-content">
+                  <h3>{ blog.title }</h3>
+                </div>
+              </Link>
+            </article>
+          ))}
+      </div>
+  }
   }
 
 export default BlogList;
