6ccda468cd99f2fb305c10d05c094da92618249d
alexwebcoder <alexdeleon@stayup.com>
2023-12-28 19:03:34 -0800

    set breakpoints on bloglist

--- src/pages/Home/BlogList.js
+++ src/pages/Home/BlogList.js
@@ -1,80 +1,44 @@
 import { Link } from "react-router-dom";
 
 const handleArticleChange = () => {
-  document.querySelector('.disabled')?.classList.remove('disabled');
-  document.querySelector('.sidenav .disabled')?.classList.remove('disabled');
-  document.querySelector('.footer .disabled')?.classList.remove('disabled');
-}
+  document.querySelector(".disabled")?.classList.remove("disabled");
+  document.querySelector(".sidenav .disabled")?.classList.remove("disabled");
+  document.querySelector(".footer .disabled")?.classList.remove("disabled");
+};
 
 const BlogList = ({ blogs, title }) => {
-  if(window.screen.width >= 321) {
-    return(
-      <div className="card-container">
-        {blogs.map((blog) => (
-          <article key={blog.id}>
-              <Link to={`/blogs/${blog.id}`} onClick={handleArticleChange}>
-              <figure className="card-img">
-              <picture>
-                  <source
-                    type="image/webp"
-                    srcSet={
-                      blog.Thum492w
-                    }
-                
-                    sizes="(max-width: 320px) 320px, 492px"
-                  />
-                  <source
-                    type="image/jpg"
-                    srcSet={
-                      blog.Thum492j
-                    }
-                    sizes="(max-width: 320px) 320px, 492px"
-                  />
-                        <img className="no-right-click" src={blog.Thum492j} alt={blog.alt}/>
-                </picture>
-              </figure>
-                <div className="blog-article-content">
-                  <h3>{ blog.title }</h3>
-                </div>
-              </Link>
-            </article>
-          ))}
-      </div>
-    );
-  }
-  if(window.screen.width <= 320) {
+  return (
     <div className="card-container">
-        {blogs.map((blog) => (
-          <article key={blog.id}>
-              <Link to={`/blogs/${blog.id}`} onClick={handleArticleChange}>
-              <figure className="card-img">
+      {blogs.map((blog) => (
+        <article key={blog.id}>
+          <Link to={`/blogs/${blog.id}`} onClick={handleArticleChange}>
+            <figure className="card-img">
               <picture>
-                  <source
-                    type="image/webp"
-                    srcSet={
-                      blog.Thum320w
-                    }
-                
-                    sizes="(max-width: 320px) 320px, 492px"
-                  />
-                  <source
-                    type="image/jpg"
-                    srcSet={
-                      blog.Thum320j
-                    }
-                    sizes="(max-width: 320px) 320px, 492px"
-                  />
-                        <img className="no-right-click" src={blog.Thum320j} alt={blog.alt}/>
-                </picture>
-              </figure>
-                <div className="blog-article-content">
-                  <h3>{ blog.title }</h3>
-                </div>
-              </Link>
-            </article>
-          ))}
-      </div>
-  }
-  }
+                <source
+                  type="image/webp"
+                  srcSet={`${blog.Thum320w} 320w, ${blog.Thum492w} 492w`}
+                  sizes="(max-width: 320px) 320px, 492px"
+                />
+                <source
+                  type="image/jpg"
+                  srcSet={`${blog.Thum320j} 320w, ${blog.Thum492j} 492w`}
+                  sizes="(max-width: 320px) 320px, 492px"
+                />
+                <img
+                  className="no-right-click"
+                  src={blog.Thum492j}
+                  alt={blog.alt}
+                />
+              </picture>
+            </figure>
+            <div className="blog-article-content">
+              <h3>{blog.title}</h3>
+            </div>
+          </Link>
+        </article>
+      ))}
+    </div>
+  );
+};
 
 export default BlogList;
