3a88ce0dab718f3d95b0fbf6cfbfc991f8ecd6dc
alexwebcoder <alexdeleon@stayup.com>
2024-01-20 18:23:29 -0800

    changed the path in newsfeed file

--- src/pages/Newsfeed/Newsfeed.js
+++ src/pages/Newsfeed/Newsfeed.js
@@ -12,7 +12,7 @@ const Newsfeed = () => {
 
   const getArticles = async () => {
     try {
-      const res = await axios.get("https://www.pickuppanama.com/");
+      const res = await axios.get("https://pickuppanama.com");
       setArticles(res.data);
     } catch (error) {}
   };
